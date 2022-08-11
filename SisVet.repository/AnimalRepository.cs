00eae491661ef96aef7392673b1546b248798665
Jandir Castilho Basegio Junior <375643@unoesc.lan>
2022-08-10 21:38:23 -0300

    first commit

---
+++ SisVet.repository/AnimalRepository.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using SisVet.Model;
+
+namespace SisVet.Repository
+{
+    public class AnimalRepository
+    {
+
+        public bool Salvar(Animal animal)
+        {
+            return true;
+        }
+
+        public Animal Retornar(int animalId)
+        {
+            Animal animal = new Animal(animalId);
+
+            if (animalId == 1)
+            {
+                animal.Nome = "toto";
+                animal.DataNascimento =DateTime.Now;
+                animal.Peso = 10;
+                animal.Altura = 05;
+                animal.Complimento = 40;
+                animal.Cor = "branco";
+                animal.Raca = new Raca();   
+
+            }
+
+            return new Animal();
+        }
+
+        public List<Animal> Retornar()
+        {
+            return new List<Animal>();
+        }
+    }
+}
+
+
+
+
---
+++ prog 1/SisVet/SisVet.Model/Raca.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SisVet.Model
+{
+    public class Raca
+    {
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+
+    }
+}
