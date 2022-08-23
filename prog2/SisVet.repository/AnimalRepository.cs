0a4860863b91eb79ca8cd07fb18917bb3d28b7e2
jandir <61996049+jandirbasegio@users.noreply.github.com>
2022-08-22 22:54:29 -0300

    Add files via upload

---
+++ prog2/SisVet.repository/AnimalRepository.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using SisVet.Model;
+
+namespace SisVet.Repository
+{
+    public class AnimalRepository
+    {
+
+        public bool Salvar(Animal animal)
+        {
+            return true;
+        }
+
+        public Animal Retornar(int animalId)
+        {
+            Animal animal = new Animal(animalId);
+
+            if (animalId == 1)
+            {
+                animal.Nome = "toto";
+                animal.DataNascimento =DateTime.Now;
+                animal.Peso = 10;
+                animal.Altura = 0.5;
+                animal.Comprimento = 0.65;
+                animal.Cor = "branco";
+                animal.Raca = new Raca();
+                animal.EnderecoId = 1;
+                animal.Endereco = new Endereco { };
+            }
+
+            return new Animal();
+        }
+
+        public List<Animal> Retornar()
+        {
+            return new List<Animal>();
+        }
+    }
+}
+
+
+
+
---
+++ prog2/SisVet.repository/ClienteRepository.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using SisVet.Model;
+
+namespace SisVet.Repository
+{
+    public class ClienteRepository
+    {
+        public bool salvar(Cliente cliente)
+        {
+            return true;
+        }
+
+        public Cliente Retornar(int clienteId)
+        {
+            Cliente cliente = new Cliente();
+
+            if (clienteId == 1)
+            {
+                cliente.Id = 1;
+                cliente.Nome = "Jandir";
+                cliente.Cpf = "123456789";
+                cliente.Email = "jandirc.basegio@gmail.com";
+                cliente.Telefone = "49991845290";
+                cliente.EnderecoId = 1;
+            }
+
+            return cliente;
+        }
+
+        public List<Cliente> Retornar()
+        {
+            return new List<Cliente>();
+        }
+    }
+}
