62fd4c0c43fdd38afdbc79d163fdecd0817cd685
jandir <61996049+jandirbasegio@users.noreply.github.com>
2022-08-22 22:27:48 -0300

    Delete SisVet.Model directory

--- SisVet.Model/Procedimento.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SisVet.Model
-{
-    public class Procedimento
-    {
-        public int Id{ get; set; }
-        public string TipoProcedimento { get; set; }
-        public string Descricao{ get; set; }
-    }
-}
